9f96d7cb39c8a851e4db00035a66c510cb71c1e9
christian reyes <creyese23@gmail.com>
2026-03-18 10:45:58 -0500

    Actualizacion del proyecto 18-03-2026 10:45

--- src/main/java/com/sena/creyese/dentvision_spring/modelo/Cita.java
+++ src/main/java/com/sena/creyese/dentvision_spring/modelo/Cita.java
@@ -1,4 +1,87 @@
 package com.sena.creyese.dentvision_spring.modelo;
 
+import jakarta.persistence.*;
+
+import javax.xml.crypto.Data;
+import java.sql.Time;
+import java.util.Date;
+
+@Entity
+@Table(name = "citas")
 public class Cita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCita;
+
+    @Column(nullable = false, length = 50)
+    private Date fecha;
+    @Column(nullable = false, length = 50)
+    private Time hora;
+    @Column(nullable = false, length = 50)
+    private boolean estado;
+    @Column(nullable = false, length = 50)
+    private String motivo;
+
+    @ManyToOne
+    @JoinColumn(name ="idPaciente")
+    private Paciente paciente;
+    @ManyToOne
+    @JoinColumn(name ="idOdontologo")
+    private Odontologo odontologo;
+
+    public Long getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(Long idCita) {
+        this.idCita = idCita;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Time getHora() {
+        return hora;
+    }
+
+    public void setHora(Time hora) {
+        this.hora = hora;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Odontologo getOdontologo() {
+        return odontologo;
+    }
+
+    public void setOdontologo(Odontologo odontologo) {
+        this.odontologo = odontologo;
+    }
 }
--- src/main/java/com/sena/creyese/dentvision_spring/modelo/Procedimiento.java
+++ src/main/java/com/sena/creyese/dentvision_spring/modelo/Procedimiento.java
@@ -1,4 +1,27 @@
 package com.sena.creyese.dentvision_spring.modelo;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.util.Date;
+
+
+@Entity
+@Table(name = "procedimientos")
 public class Procedimiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProcedimiento;
+
+    @NotBlank(message = "")
+    @Column(nullable = false, length = 50)
+    private String descripcion;
+    private boolean estado;
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+
+    @ManyToOne
+    @JoinColumn(name ="idCita")
+    private Cita cita;
 }
